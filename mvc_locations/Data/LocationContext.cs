08fe5814ed7690402ac86bd609e1c1f693a92a3f
git@nekic.org <git@nekic.org>
2022-03-30 16:28:52 +0200

    Add project files.

---
+++ mvc_locations/Data/LocationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_locations.Models;
+
+namespace mvc_locations.Data
+{
+    public class LocationContext : DbContext
+    {
+
+        public LocationContext(DbContextOptions<LocationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        // Zadatak:
+        // kreirati metodu OnModelCreating(ModelBuilder modelBuilder)
+        // Napraviti seed podataka za:
+        // Minimalno dvije nasumične države s pripadajućim podacima
+        // minimalno šest nasumičnih gradova s pripadajućim podacima
+    }
+}
